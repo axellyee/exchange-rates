9f9a298f2d035cbc93ef241df3f2c96270396ac3
Aksels <akselsrei@gmail.com>
2020-02-15 18:42:08 +0200

    Added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        fontFamily: {
+            'sans': ['Roboto', 'sans-serif'],
+        },
+    },
+
+    variants: {},
+    plugins: [],
+
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
+require('laravel-mix-purgecss');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,4 +14,25 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+    ]);
+
+// start up a live updating server
+if (! mix.inProduction()) {
+    mix.browserSync('localhost:8000')
+        .disableSuccessNotifications();
+}
+
+// optimize for production
+if (mix.inProduction()) {
+    mix.purgeCss({
+        // Specify the paths to all of the template files in your project
+        content: [
+            './src/**/*.html',
+        ],
+        css: ['**/*.css'],
+        js: ['**/*.js'],
+    })
+        .version();
+}
